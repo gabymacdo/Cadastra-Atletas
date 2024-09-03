d2751d29d600ad50db72dd3b33af30e27fb45225
gabymacedo2504 <151886270+gabymacedo2504@users.noreply.github.com>
2024-09-03 20:22:18 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Página Inicial</title>
+</head>
+<body>
+    <h1>Bem-vindo ao Sistema de Cadastro de Atletas</h1>
+    
+    <nav>
+        <ul>
+            <li><a href="cadastro_atletas.php">Cadastrar Atleta</a></li>
+            <li><a href="listar_atletas.php">Listar Atletas</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
